113800dcf2b24987cd6c257f665e37f21efcff61
SaurabhKarki-25 <saurabhsinghkarkisaurabh10@gmail.com>
2026-01-24 12:55:25 +0530

    server update

--- server.js
+++ server.js
@@ -23,20 +23,18 @@ const app = express();
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
 /* ======================= MIDDLEWARE ======================= */
-app.use(
-  cors({
-    origin: [
-      "https://chat-application-frontend-1s54-frnguq019.vercel.app",
-      "http://localhost:5173",
-    ],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+const corsOptions = {
+  origin: [
+    "https://chat-application-frontend-1s54-frnguq019.vercel.app",
+    "http://localhost:5173",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-// ✅ Fix for Express v5
-app.options("/*", cors());
+app.use(cors(corsOptions));
+
 
 
 app.use(express.json({ limit: "10mb" }));
