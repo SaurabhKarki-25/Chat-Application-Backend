8e07fd9b457b4b842e51acc1a105d2629202009e
SaurabhKarki-25 <saurabhsinghkarkisaurabh10@gmail.com>
2026-01-24 13:02:31 +0530

    server update

--- server.js
+++ server.js
@@ -19,21 +19,29 @@ connectDB();
 
 const app = express();
 
-/* ======================= CONFIG ======================= */
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
-/* ======================= MIDDLEWARE ======================= */
-const corsOptions = {
-  origin: [
-    "https://chat-application-frontend-1s54-frnguq019.vercel.app",
-    "http://localhost:5173",
-  ],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
 
-app.use(cors(corsOptions));
+const allowedOrigins = [
+  "https://chat-application-frontend-1s54-frngvq019.vercel.app",
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); // Postman / server-to-server
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("CORS blocked: " + origin));
+    },
+    credentials: true,
+  })
+);
+
+
 
 
 
