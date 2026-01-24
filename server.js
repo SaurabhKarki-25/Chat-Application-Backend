779b9deee09f8eb8d4f595bc07a4034ef8d253eb
SaurabhKarki-25 <saurabhsinghkarkisaurabh10@gmail.com>
2026-01-24 12:52:53 +0530

    server update

--- server.js
+++ server.js
@@ -25,15 +25,19 @@ const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 /* ======================= MIDDLEWARE ======================= */
 app.use(
   cors({
-    origin: [FRONTEND_URL],
+    origin: [
+      "https://chat-application-frontend-1s54-frnguq019.vercel.app",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
-// ✅ preflight
-app.options("*", cors());
+// ✅ Fix for Express v5
+app.options("/*", cors());
+
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
